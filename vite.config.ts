c76213df19e9e4951d91907e555d2c726919a40e
narip <narip@github.com>
2024-02-03 18:11:20 +0900

    vite init

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    /** 포트가 이미 사용 중일 때 다른 포트를 사용하게끔 설정 */
+    strictPort: false,
+  }
+})
