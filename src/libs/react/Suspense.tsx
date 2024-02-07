855967bde9e0073d60aeddbe523f0fecb553f9fd
narip <narip@github.com>
2024-02-07 14:44:09 +0900

    feat: Suspense 컴포넌트 추가, qureyKey 추가
    - Suspense: api 가 너무 빠른 경우 깜빡거림을 방지하기 위해서 최소 1.5초는 로더를 보여준다.
    - queryKey 를 효울적으로 사용하기 위해서 react-query 도큐먼트에서 알려준 라이브러리를 사용한다

---
+++ src/libs/react/Suspense.tsx
@@ -0,0 +1,21 @@
+import { Suspense as RSuspense, useEffect, useState } from 'react';
+import { PageLoader } from '@/components/page';
+import type { ReactNode } from 'react';
+
+interface SuspenseProps {
+  /** 지연 시간(ms) */
+  delay?: number;
+  fallback?: ReactNode;
+  children: ReactNode;
+}
+const Suspense = ({ delay = 1500, fallback = <PageLoader />, children }: SuspenseProps) => {
+  const [isDelay, setIsDelay] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsDelay(false), delay);
+  }, [delay]);
+
+  return <RSuspense fallback={fallback}>{isDelay ? fallback : children}</RSuspense>;
+};
+
+export default Suspense;
